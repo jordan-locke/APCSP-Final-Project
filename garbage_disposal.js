352d21643dd4039d52b3d00fcc56af99a3ac41ad
Jordan H Locke <jl64449@srcs.loc>
2019-04-16 14:41:01 -0700

    game class functions

--- garbage_disposal.js
+++ garbage_disposal.js
@@ -3,13 +3,31 @@ class Game {
         this.player = new Player();
         this.score = 0;
         this.timer = 10;
-        this.plasticItems = [];
-        this.compostItems = [];
-        this.paperItems = [];
+        this.plastic1 = [];
+        this.compost2 = [];
+        this.paper3 = [];
         this.currentTrash = null;
     }
 
     pickRandomTrash() {
+        let randomNumber = Math.ceil(Math.random()*3);
+        if (randomNumber == 1) {
+            let randomPlastic = Math.floor(Math.random()*this.plastic1.length);
+            this.currentTrash = this.plastic1[randomPlastic];
+        } else if (randomNumber == 2) {
+            let randomCompost = Math.floor(Math.random()*this.compost2.length);
+            this.currentTrash = this.compost2[randomCompost];
+        } else {
+            let randomPaper = Math.floor(Math.random()*this.paper3.length);
+            this.currentTrash = this.paper3[randomPaper];
+        }
+    }
 
+    addToScore() {
+        this.score = this.score + 1;
+    }
+
+    logTimer() {
+        document.getElementById("scoreboard");
     }
 }
