81b805ed61753836cff10301b3022a6883f523b0
Jordan H Locke <jl64449@srcs.loc>
2019-04-16 14:22:17 -0700

    creation of trash, player js files; html div creation

---
+++ garbage_disposal.js
@@ -0,0 +1,15 @@
+class Game {
+    constructor() {
+        this.player = new Player();
+        this.score = 0;
+        this.timer = 10;
+        this.plasticItems = [];
+        this.compostItems = [];
+        this.paperItems = [];
+        this.currentTrash = null;
+    }
+
+    pickRandomTrash() {
+
+    }
+}
