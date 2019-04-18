bec581b599196092ceb31d66575d938e313ea2da
Jonk4e21 <jonathan.kfouri@me.com>
2019-04-17 23:40:26 -0700

    Trash Class first code

--- trash.js
+++ trash.js
@@ -1,1 +1,71 @@
-class Trash {}
+class Trash {
+    constructor(_photo) {
+        this.photo = _photo;
+    }
+}
+
+let plastic = [
+    waterBottle,
+    cup,
+    packaging,
+    yogurtCup,
+    waterJug,
+    juiceJug,
+    container,
+    plasticTube
+]
+
+var waterBottle = new Trash(https://4.imimg.com/data4/EU/YB/MY-6282801/normal-plastic-bottle-500x500.jpg);
+var cup = new Trash(https://sc02.alicdn.com/kf/UT8D0v_XthaXXagOFbX6/Wholelsale-Disposable-PET-Plastic-Cup-with-lids.jpg);
+var packaging = new Trash(https://i5.walmartimages.com/asr/ad65e07f-a701-41f3-8a20-357ed5507e84_1.827c65affb84303b7d2c64a5732a4681.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF);
+var yogurtCup = new Trash(https://oembargain.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/5/15s-b_1.jpg);
+var waterJug = new Trash(https://images-na.ssl-images-amazon.com/images/I/41geyS%2B3FwL.jpg);
+var juiceJug = new Trash(http://ecx.images-amazon.com/images/I/310tRm6gYsL.jpg);
+var container = new Trash(http://www.castawayfoodpackaging.com.au/wp-content/uploads/CA-CM650_WEB_A.png);
+var plasticTube = new Trash(https://cdn.shopify.com/s/files/1/2612/8356/products/plastic-tubes-for-vape-cartridges-12mm-x-85mm-packaging-container-white-4011282104402.jpg?v=1547475054);
+
+let paper = [
+    bigBox,
+    smallBox,
+    crumpledPaper,
+    flatPaper,
+    tissue,
+    cardboardTube,
+    envelope,
+    card,
+    paperAirplane,
+]
+
+var bigBox = new Trash();
+var smallBox = new Trash();
+var crumpledPaper = new Trash();
+var flatPaper = new Trash();
+var tissue = new Trash();
+var cardboardTube = new Trash();
+var envelope = new Trash();
+var card = new Trash();
+var paperAirplane = new Trash();
+
+let compost = [
+    bananaPeel,
+    coffeeFilter,
+    teaBag,
+    appleCore,
+    avacadoRind,
+    orangePeel,
+    leaf,
+    stick,
+    dirt,
+    eggShells,
+]
+
+var bananaPeel = new Trash();
+var coffeeFilter = new Trash();
+var teaBag = new Trash();
+var appleCore = new Trash():
+var avacadoRind = new Trash();
+var orangePeel = new Trash();
+var leaf = new Trash();
+var stick = new Trash();
+var dirt = new Trash();
+var eggshells = new Trash();
