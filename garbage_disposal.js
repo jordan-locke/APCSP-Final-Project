301e489966bea8f22a6f772452476a6487d50662
Jordan H Locke <jl64449@srcs.loc>
2019-04-30 14:35:52 -0700

    GAME WORKS; ALGORITHMS ARE DONE

--- garbage_disposal.js
+++ garbage_disposal.js
@@ -11,11 +11,13 @@ class Game {
     }
 
     startGame() {
-        console.log("in startGame");
+        console.log("in startGame", this.timer);
         if (startButton.style.visibility == "hidden") {
             console.log(this.currentTrash);
         } else {
             startButton.style.visibility = "hidden";
+            this.endOfGame = false;
+            this.timer = 10;
             document.getElementById("score-input").textContent = "0";
             document.getElementById("timer-input").textContent = "10";
             document.getElementById("pop-up").style.visibility = "hidden";
@@ -139,7 +141,7 @@ class Game {
     }
 
     logTimer() {
-        console.log("in logTimer");
+        console.log("in logTimer", this.timer);
         if (this.score >= 99) {
             console.log("this.score >= 99");
             this.timerMax = 1;
@@ -149,7 +151,8 @@ class Game {
         } else if (this.correctAnswer == true) {
             this.timer = this.timerMax;
             console.log("correctAnswer", this.timer);
-        } console.log("end of logTimer", this.timer);
+        }
+        console.log("end of logTimer", this.timer);
     }
 
     endGame(id) {
@@ -201,14 +204,14 @@ class Trash {
 }
 
 
-var waterBottle = new Trash("water bottle", "https://4.imimg.com/data4/EU/YB/MY-6282801/normal-plastic-bottle-500x500.jpg", 3);
-var cup = new Trash("cup", "https://sc02.alicdn.com/kf/UT8D0v_XthaXXagOFbX6/Wholelsale-Disposable-PET-Plastic-Cup-with-lids.jpg", 3);
-var packaging = new Trash("packaging", "https://i5.walmartimages.com/asr/ad65e07f-a701-41f3-8a20-357ed5507e84_1.827c65affb84303b7d2c64a5732a4681.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF", 3);
-var yogurtCup = new Trash("yogurt cup", "https://oembargain.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/5/15s-b_1.jpg", 3);
-var waterJug = new Trash("water jug", "https://images-na.ssl-images-amazon.com/images/I/41geyS%2B3FwL.jpg", 3);
-var juiceJug = new Trash("juice jug", "http://ecx.images-amazon.com/images/I/310tRm6gYsL.jpg", 3);
-var container = new Trash("container", "http://www.castawayfoodpackaging.com.au/wp-content/uploads/CA-CM650_WEB_A.png", 3);
-var plasticTube = new Trash("plastic tube", "https://cdn.shopify.com/s/files/1/2612/8356/products/plastic-tubes-for-vape-cartridges-12mm-x-85mm-packaging-container-white-4011282104402.jpg?v=1547475054", 3);
+var waterBottle = new Trash(0, 0, 0, 0, "water bottle", "https://4.imimg.com/data4/EU/YB/MY-6282801/normal-plastic-bottle-500x500.jpg", 3, "water-bottle");
+var cup = new Trash(0, 0, 0, 0, "cup", "https://sc02.alicdn.com/kf/UT8D0v_XthaXXagOFbX6/Wholelsale-Disposable-PET-Plastic-Cup-with-lids.jpg", 3, "cup");
+var packaging = new Trash(0, 0, 0, 0, "packaging", "https://i5.walmartimages.com/asr/ad65e07f-a701-41f3-8a20-357ed5507e84_1.827c65affb84303b7d2c64a5732a4681.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF", 3, "packaging");
+var yogurtCup = new Trash(0, 0, 0, 0, "yogurt cup", "https://oembargain.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/1/5/15s-b_1.jpg", 3, "yogurt-cup");
+var waterJug = new Trash(0, 0, 0, 0, "water jug", "https://images-na.ssl-images-amazon.com/images/I/41geyS%2B3FwL.jpg", 3, "water-jug");
+var juiceJug = new Trash(0, 0, 0, 0, "juice jug", "http://ecx.images-amazon.com/images/I/310tRm6gYsL.jpg", 3, "juice-jug");
+var container = new Trash(0, 0, 0, 0, "container", "http://www.castawayfoodpackaging.com.au/wp-content/uploads/CA-CM650_WEB_A.png", 3, "food-container");
+var plasticTube = new Trash(0, 0, 0, 0, "plastic tube", "https://cdn.shopify.com/s/files/1/2612/8356/products/plastic-tubes-for-vape-cartridges-12mm-x-85mm-packaging-container-white-4011282104402.jpg?v=1547475054", 3, "plastic-tube");
 
 let plastic3 = [
     waterBottle,
@@ -221,16 +224,16 @@ let plastic3 = [
     plasticTube
 ]
 
-var bananaPeel = new Trash("banana peel", "https://www.thedailymeal.com/sites/default/files/story/2016/bananapeel.JPG", 2);
-var coffeeFilter = new Trash("coffee filter", "https://www.sciencedaily.com/images/2015/05/150513112035_1_900x600.jpg", 2);
-var teaBag = new Trash("tea bag", "https://cdn.shopify.com/s/files/1/0654/3125/products/TB_b6af5482-c6ec-47b1-ab2f-adc20f69d12a_grande.jpg?v=1426877660", 2);
-var appleCore = new Trash("apple core", "https://progressive.org/downloads/5300/download/rotten%20apple%20.jpg.jpe?cb=c4a7db57c9e999ed5e304327da730ae3", 2);
-var avocadoRind = new Trash("avocado rind", "https://daily.jstor.org/wp-content/uploads/2017/05/avocado_1050x700.jpg", 2);
-var orangePeel = new Trash("orange peel", "http://assets.stickpng.com/thumbs/5a68f916988f2a795ef76ce3.png", 2);
-var leaf = new Trash("leaf", "https://www.ctfresh.com.sg/wp-content/uploads/2017/11/177130309.jpg", 2);
-var stick = new Trash("stick", "http://www.stickpng.com/assets/images/580b585b2edbce24c47b26c7.png", 2);
-var dirt = new Trash("dirt", "https://static.canadiancattlemen.ca/wp-content/uploads/2015/02/153935374.jpg", 2);
-var eggShells = new Trash("egg shells", "http://www.stickpng.com/assets/thumbs/5c570e158c21c9029a0f48c1.png", 2);
+var bananaPeel = new Trash(0, 0, 0, 0, "banana peel", "https://www.thedailymeal.com/sites/default/files/story/2016/bananapeel.JPG", 2, "banana-peel");
+var coffeeFilter = new Trash(0, 0, 0, 0, "coffee filter", "https://www.sciencedaily.com/images/2015/05/150513112035_1_900x600.jpg", 2, "coffee-filter");
+var teaBag = new Trash(0, 0, 0, 0, "tea bag", "https://cdn.shopify.com/s/files/1/0654/3125/products/TB_b6af5482-c6ec-47b1-ab2f-adc20f69d12a_grande.jpg?v=1426877660", 2, "tea-bag");
+var appleCore = new Trash(0, 0, 0, 0, "apple core", "https://progressive.org/downloads/5300/download/rotten%20apple%20.jpg.jpe?cb=c4a7db57c9e999ed5e304327da730ae3", 2, "apple-core");
+var avocadoRind = new Trash(0, 0, 0, 0, "avocado rind", "https://daily.jstor.org/wp-content/uploads/2017/05/avocado_1050x700.jpg", 2, "avocado");
+var orangePeel = new Trash(0, 0, 0, 0, "orange peel", "http://assets.stickpng.com/thumbs/5a68f916988f2a795ef76ce3.png", 2, "orange-peel");
+var leaf = new Trash(0, 0, 0, 0, "leaf", "https://www.ctfresh.com.sg/wp-content/uploads/2017/11/177130309.jpg", 2, "leaf");
+var stick = new Trash(0, 0, 0, 0, "stick", "http://www.stickpng.com/assets/images/580b585b2edbce24c47b26c7.png", 2, "stick");
+var dirt = new Trash(0, 0, 0, 0, "dirt", "https://static.canadiancattlemen.ca/wp-content/uploads/2015/02/153935374.jpg", 2, "dirt");
+var eggShells = new Trash(0, 0, 0, 0, "egg shells", "http://www.stickpng.com/assets/thumbs/5c570e158c21c9029a0f48c1.png", 2, "egg-shells");
 
 let compost2 = [
     bananaPeel,
@@ -245,15 +248,15 @@ let compost2 = [
     eggShells,
 ]
 
-var bigBox = new Trash("big box", "https://s3-ap-southeast-2.amazonaws.com/wc-prod-pim/JPEG_300x300/VISSC280_shipping_carton_280_x_255_x_215mm_15_pack.jpg", 1);
-var smallBox = new Trash("small box", "https://ae01.alicdn.com/kf/HTB1PtLMIpXXXXb8XpXXq6xXFXXXA/Carton-Box-Paper-Boxes-Neoprene-Swimwear-Bikini-Clothing-Packing-Boxes-Anti-Wrinkle-Hard-Brown-color.jpg_640x640.jpg", 1);
+var bigBox = new Trash(0, 0, 0, 0, "big box", "https://s3-ap-southeast-2.amazonaws.com/wc-prod-pim/JPEG_300x300/VISSC280_shipping_carton_280_x_255_x_215mm_15_pack.jpg", 1, "big-box");
+var smallBox = new Trash(0, 0, 0, 0, "small box", "https://ae01.alicdn.com/kf/HTB1PtLMIpXXXXb8XpXXq6xXFXXXA/Carton-Box-Paper-Boxes-Neoprene-Swimwear-Bikini-Clothing-Packing-Boxes-Anti-Wrinkle-Hard-Brown-color.jpg_640x640.jpg", 1, "small-box");
 //var crumpledPaper = new Trash("crumpled paper", "https://media.gettyimages.com/photos/crumpled-paper-ball-picture-id182906514?b=1&k=6&m=182906514&s=612x612&w=0&h=AWrFkSlsZWxmmr_vxmi94ABPCNIHgXAiHvnfejYCei8=", 1);
-var flatPaper = new Trash("flat paper", "https://cdn1.bigcommerce.com/server4300/c7561/products/73/images/1735/paper_sheet__41145.1424302341.380.380.jpg?c=2", 1);
-var tissue = new Trash("tissue", "https://img1.exportersindia.com/product_images/bc-full/dir_5/126172/tissue-paper-1521700403-76103.jpeg", 1);
-var cardboardTube = new Trash("cardboard tube", "https://housewifehowtos.com/wp-content/uploads/2012/05/toilet-paper-cardboard-tube.jpg", 1);
-var envelope = new Trash("envelope", "https://s3.amazonaws.com/static.lcipaper.com/img/prod/AGD7E-OUTmed.png", 1);
-var card = new Trash("card", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/LeibnizBrief1.jpg/200px-LeibnizBrief1.jpg", 1);
-var paperAirplane = new Trash("paper airplane", "http://activehistory.ca/wp-content/uploads/2018/05/1024px-Paperairplane-1024x573.png", 1);
+var flatPaper = new Trash(0, 0, 0, 0, "flat paper", "https://cdn1.bigcommerce.com/server4300/c7561/products/73/images/1735/paper_sheet__41145.1424302341.380.380.jpg?c=2", 1, "flat-paper");
+var tissue = new Trash(0, 0, 0, 0, "tissue", "https://img1.exportersindia.com/product_images/bc-full/dir_5/126172/tissue-paper-1521700403-76103.jpeg", 1, "tissue");
+var cardboardTube = new Trash(0, 0, 0, 0, "cardboard tube", "https://housewifehowtos.com/wp-content/uploads/2012/05/toilet-paper-cardboard-tube.jpg", 1, "cardboard-tube");
+var envelope = new Trash(0, 0, 0, 0, "envelope", "https://s3.amazonaws.com/static.lcipaper.com/img/prod/AGD7E-OUTmed.png", 1, "envelope");
+var card = new Trash(0, 0, 0, 0, "card", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/LeibnizBrief1.jpg/200px-LeibnizBrief1.jpg", 1, "card");
+var paperAirplane = new Trash(0, 0, 0, 0, "paper airplane", "http://activehistory.ca/wp-content/uploads/2018/05/1024px-Paperairplane-1024x573.png", 1, "paper-airplane");
 
 let paper1 = [
     bigBox,
